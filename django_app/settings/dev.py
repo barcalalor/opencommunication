eaa168c3bb5ffb7e6e440e6af6dc297652c0dd60
jbarcala <javier.barcala@grupoica.com>
2021-01-24 17:47:49 +0100

    Add Celery to handle posible periodic tasks

--- django_app/settings/dev.py
+++ django_app/settings/dev.py
@@ -7,3 +7,6 @@ from django_app.settings.base import *
 DEBUG = True
 
 DATABASES['default'] = dj_database_url.config(default=DATABASE_URL)
+CELERY_TIMEZONE = "Europe/Madrid"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
--- django_app/settings/prod.py
+++ django_app/settings/prod.py
@@ -7,3 +7,6 @@ from django_app.settings.base import *
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DEBUG=False
 DATABASES['default'] = dj_database_url.config(default=DATABASE_URL)
+CELERY_TIMEZONE = "Europe/Madrid"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
